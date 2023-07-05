a9dc3ca03441069b7d1c5f2cc332c5bd7792703b
Lalit <lalitkumaran1602@gmail.com>
2023-07-05 19:31:40 +0530

    0.21

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Lalit.pdf";
+import pdf from "../../Assets/Lalit.pdf";
+import cv from "../../Assets/CV.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
@@ -23,7 +24,7 @@ function ResumeNew() {
         </h1>
 
         <Row className="resume">
-          <Document file={pdf} className="d-flex justify-content-center">
+          <Document file={cv} className="d-flex justify-content-center">
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
@@ -39,17 +40,17 @@ function ResumeNew() {
             &nbsp;Download Profile
           </Button>
         </Col>
-        {/* <Col style={{ justifyContent: "center", position: "relative" }}>
+        <Col style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
-            href={pdf}
+            href={cv}
             target="_blank"
             style={{ maxWidth: "250px" }}
           >
             <AiOutlineDownload />
             &nbsp;Download CV
           </Button>
-        </Col> */}
+        </Col>
         </Row>
       </Container>
     </div>
